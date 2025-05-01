bb8658915f7666cbf3f056204626d9785aaf8ebd
Cássio Jandir Pagnoncelli <cassiopagnoncellI@gmail.com>
2025-05-01 19:55:35 -0300

    Add db, console, dev, prod commands

---
+++ template/app/server.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from routers import *
+
+app = FastAPI(title="Hero API")
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+for router_name in dir():
+  if router_name.endswith('_router') and isinstance(globals()[router_name], APIRouter):
+    app.include_router(globals()[router_name])
+
+app.mount("/public", StaticFiles(directory="public"), name="public")
