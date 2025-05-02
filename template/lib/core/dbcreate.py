31d31e4d0a897e5186a26b129c5178610648e477
Cássio Jandir Pagnoncelli <cassiopagnoncellI@gmail.com>
2025-05-02 04:15:35 -0300

    Serp

--- template/lib/core/dbcreate.py
+++ template/lib/core/dbcreate.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
 from sqlmodel import Session, SQLModel
 from config.initializers.database import get_session_standalone, engine
 
