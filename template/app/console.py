6d8ab587474453557d6bd0c2cb817de30417c738
Cássio Jandir Pagnoncelli <cassiopagnoncellI@gmail.com>
2025-05-04 15:11:10 -0300

    Add storage configs

--- template/app/console.py
+++ template/app/console.py
@@ -1,3 +1,13 @@
+import sys
+import os
+from os import getenv
+from pathlib import Path
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, project_root)
+
+import lib.core.env
+
 def print_colored_snake():
   GREEN = "\033[32m"
   RED = "\033[31m"
@@ -27,21 +37,14 @@ def print_colored_snake():
       print(line)
 
 if __name__ == "__main__":
-  import sys
-  import os
-  
-  # Add the project root to Python path
-  project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-  sys.path.insert(0, project_root)
-  
-  print_colored_snake()
-  
-  # Import Python libraries
+  from IPython.terminal.embed import InteractiveShellEmbed
+  from traitlets.config import Config
   from sqlmodel import SQLModel, select, update
 
-  # Import environment variables
-  import lib.core.env
-  from os import getenv
+  # Import project initializers
+  from config.initializers.database import get_session_standalone
+  from config.initializers.redis import RedisStandaloneDep
+  from config.initializers.storage import *
 
   # Import project modules
   from app.jobs import *
@@ -49,18 +52,23 @@ if __name__ == "__main__":
   from app.services import *
   from app.utils import *
 
-  # Import project initializers
-  from config.initializers.database import get_session_standalone
-  from config.initializers.redis import RedisStandaloneDep
-  from config.initializers.storage import storage
+  print_colored_snake()
+
+  c = Config()
+  c.TerminalInteractiveShell.confirm_exit = False
+  c.InteractiveShellEmbed.colors = "Linux"        # 'NoColor', 'LightBG', 'Linux'
+  c.InteractiveShellEmbed.autocall = 2            # Auto-call functions (like Rails console)
+  c.TerminalInteractiveShell.editing_mode = "vi"  # Optional: vi mode
+
+  # Setup history (save command history between sessions)
+  ipython_dir = Path.home() / ".config" / "serp-ipython"
+  ipython_dir.mkdir(parents=True, exist_ok=True)
+  os.environ["IPYTHONDIR"] = str(ipython_dir)
 
   # Start IPython REPL
-  from IPython import embed
-  from IPython.terminal.interactiveshell import TerminalInteractiveShell
-  TerminalInteractiveShell.banner1 = ""
-  TerminalInteractiveShell.banner2 = ""
-  print("\nType 'exit()' or ^D to quit\n")
-
+  print("\n💻 Starting interactive console. Type 'exit()' or press Ctrl-D to quit.\n")
   with RedisStandaloneDep() as redis:
     with get_session_standalone() as db:
-      embed(ipython=True, no_confirm_exit=True, no_banner=True, display_banner=False)
+      local_vars = { "db": db, "redis": redis }
+      shell = InteractiveShellEmbed(config=c, banner1="📦 Console loaded", exit_msg="👋 Goodbye!")
+      shell(local_ns=local_vars)
--- template/config/initializers/storage.py
+++ template/config/initializers/storage.py
@@ -1,51 +1,39 @@
-from os import getenv
+from os import environ
+from jinja2 import Environment, FileSystemLoader
+from ruamel.yaml import YAML
+
+storage_env = Environment(loader=FileSystemLoader('.'), autoescape=False)
+storage_template = storage_env.get_template("config/storage.yml")
+storage_rendered = storage_template.render(**environ)
+storage_config = YAML().load(open("config/storage.yml"))
+
 from minio import Minio
 from minio.error import S3Error
 
-# Create the MinIO client
-client = Minio(
-    endpoint=getenv('MINIO_ENDPOINT_URL').replace('http://', '').replace('https://', ''),
-    access_key=getenv('MINIO_ACCESS_KEY'),
-    secret_key=getenv('MINIO_SECRET_KEY'),
-    secure=getenv('MINIO_ENDPOINT_URL').startswith('https')
-)
+# client = Minio(
+#   endpoint = getenv('MINIO_ENDPOINT_URL').replace('http://', '').replace('https://', ''),
+#   access_key = getenv('MINIO_ACCESS_KEY'),
+#   secret_key = getenv('MINIO_SECRET_KEY'),
+#   secure = getenv('MINIO_ENDPOINT_URL').startswith('https')
+# )
 
-bucket = 'resumee-production'
+# bucket = 'resumee-production'
 
-# Ensure the bucket exists
-if not client.bucket_exists(bucket):
-    client.make_bucket(bucket)
+# # Ensure the bucket exists
+# if not client.bucket_exists(bucket):
+#     client.make_bucket(bucket)
 
-# Upload a file
-local_upload_path = 'alembic.ini'
-remote_object_name = 'alembic.txt'
+# # Upload a file
+# local_upload_path = 'alembic.ini'
+# remote_object_name = 'alembic.txt'
 
-client.fput_object(bucket, remote_object_name, local_upload_path)
-print(f"✅ Uploaded {local_upload_path} to {bucket}/{remote_object_name}")
+# # client.fput_object(bucket, remote_object_name, local_upload_path)
+# # print(f"✅ Uploaded {local_upload_path} to {bucket}/{remote_object_name}")
 
-# Download the same file
-local_download_path = 'downloaded_alembic.ini'
+# # # Download the same file
+# # local_download_path = 'downloaded_alembic.ini'
 
-client.fget_object(bucket, remote_object_name, local_download_path)
-print(f"✅ Downloaded {bucket}/{remote_object_name} to {local_download_path}")
+# # client.fget_object(bucket, remote_object_name, local_download_path)
+# # print(f"✅ Downloaded {bucket}/{remote_object_name} to {local_download_path}")
 
-storage = client
-
-# class StorageS3:
-#   def __init__(self):
-#     self.client = client
-#     self.bucket = bucket
-
-#   def upload_file(self, local_upload_path, remote_object_name):
-#     self.client.fput_object(self.bucket, remote_object_name, local_upload_path)
-#     print(f"✅ Uploaded {local_upload_path} to {self.bucket}/{remote_object_name}")
-
-#   def download_file(self, remote_object_name, local_download_path):
-#     self.client.fget_object(self.bucket, remote_object_name, local_download_path)
-#     print(f"✅ Downloaded {self.bucket}/{remote_object_name} to {local_download_path}")
-
-#   def get_client(self):
-#     return self.client
-
-#   def get_bucket(self):
-#     return self.bucket
+# storage = client
