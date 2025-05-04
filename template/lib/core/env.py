9871da6c8e4e7c0e2f09019bf29600006bcc7184
Cássio Jandir Pagnoncelli <cassiopagnoncellI@gmail.com>
2025-05-04 17:23:37 -0300

    Fix env py

--- template/lib/core/env.py
+++ template/lib/core/env.py
@@ -8,13 +8,14 @@ if APP_ENV not in ['development', 'test', 'staging', 'production']:
   raise ValueError(f"Invalid app environment: {APP_ENV}")
 
 # Load environment variables from file
-if file_exists(Path().absolute() / f".env.{APP_ENV}"):
-  load_dotenv(dotenv_path = Path().absolute() / f".env.{APP_ENV}")
+env_file = Path().absolute() / f".env.{APP_ENV}"
+if env_file.exists():
+  load_dotenv(dotenv_path = env_file)
 else:
   raise FileNotFoundError(f"Instead of using .env environment file, please use .env.{APP_ENV} file")
 
 # Define app environment variables
-app_env_development = APP_ENV == 'development'
-app_env_test = APP_ENV == 'test'
-app_env_staging = APP_ENV == 'staging'
-app_env_production = APP_ENV == 'production'
+is_development = APP_ENV == 'development'
+is_test = APP_ENV == 'test'
+is_staging = APP_ENV == 'staging'
+is_production = APP_ENV == 'production'
